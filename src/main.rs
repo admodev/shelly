a91bc47ef04dc2c5cbbb215a6000b91546aacc60
admodev <admodevcodes@outlook.com>
2022-08-09 01:41:04 -0300

    interactive shell

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,69 @@
-use std::io::stdin;
-use std::io::stdout;
-use std::io::Write;
-use std::process::Command;
+  use std::env;
+  use std::io::{stdin, stdout, Write};
+  use std::path::Path;
+  use std::process::{Child, Command, Stdio};
+  
+  fn main(){
+    loop {
+      print!("> ");
+      stdout().flush().unwrap();
+      
+      let mut input = String::new();
+      stdin().read_line(&mut input).unwrap();
 
-fn main() {
-  loop {
-    // Cmd prompt character
-    print!("> ");
-    stdout().flush();
+      let mut commands = input.trim().split(" | ").peekable();
+      let mut previous_command = None;
+      
+      while let Some(command) = commands.next()  {
 
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    
-    // Trim trailing newline
-    let command = input.trim();
-    
-    let mut child = Command::new(command)
-        .spawn()
-        .unwrap();
-
-    // Prevent accepting commands before the actual cmd completes
-    child.wait();
+        let mut parts = command.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+        
+        match command {
+          "cd" => {
+            // default to '/' if a new dir is not provided
+            let new_dir = args.peekable().peek().map_or("/", |x| *x);
+            let root = Path::new(new_dir);
+            if let Err(e) = env::set_current_dir(&root) {
+              eprintln!("{}", e);
+            }
+            
+            previous_command = None;
+          },
+          "exit" => return,
+          command => {
+            let stdin = previous_command
+            .map_or(Stdio::inherit(),
+            |output: Child| Stdio::from(output.stdout.unwrap()));
+            
+            let stdout = if commands.peek().is_some() {
+              Stdio::piped()
+            } else {
+              // send output to stdout
+              Stdio::inherit()
+            };
+            
+            let output = Command::new(command)
+            .args(args)
+            .stdin(stdin)
+            .stdout(stdout)
+            .spawn();
+            
+            match output {
+              Ok(output) => { previous_command = Some(output); },
+              Err(e) => {
+                previous_command = None;
+                eprintln!("{}", e);
+              },
+            };
+          }
+        }
+      }
+      
+      if let Some(mut final_command) = previous_command {
+        // block shell until final command has finished
+        final_command.wait().unwrap();
+      }
+    }
   }
-}
